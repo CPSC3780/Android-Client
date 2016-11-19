f31f702280b3f8b3e1c44fa590fe6bf4d6b193bb
TigleyM <tigleym@gmail.com>
2016-11-18 22:01:22 -0700

    WIP

---
+++ app/src/main/java/com/example/micah/cpsc3780_ndk/Client.java
@@ -0,0 +1,88 @@
+package com.example.micah.cpsc3780_ndk;
+/**
+ * Created by Micah on 2016-11-18.
+ */
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+// TODO: this code from http://androidsrc.net/android-client-server-using-sockets-client-implementation/
+// TODO: will act as a template for the client. Will adjust accordingly
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+    String dstAddress;
+    int dstPort;
+    String response = "";
+    TextView textResponse;
+
+    Client(String addr, int port, TextView textResponse) {
+        dstAddress = addr;
+        dstPort = port;
+        this.textResponse = textResponse;
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(dstAddress, dstPort);
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
+                    1024);
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            InputStream inputStream = socket.getInputStream();
+
+         /*
+          * notice: inputStream.read() will block if no data return
+          */
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+                response += byteArrayOutputStream.toString("UTF-8");
+            }
+
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "UnknownHostException: " + e.toString();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            response = "IOException: " + e.toString();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        textResponse.setText(response);
+        super.onPostExecute(result);
+    }
+
+}
--- app/src/main/java/com/example/micah/cpsc3780_ndk/MainActivity.java
+++ app/src/main/java/com/example/micah/cpsc3780_ndk/MainActivity.java
@@ -3,22 +3,57 @@ package com.example.micah.cpsc3780_ndk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
+    TextView response;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    /**
+     * App entry point, here we are instantiating UI elements
+     * and passing event handlers accordingly
+     */
+        editTextAddress = (EditText) findViewById(R.id.addressEditText);
+        editTextPort = (EditText) findViewById(R.id.portEditText);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
+        response = (TextView) findViewById(R.id.responseTextView);
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client(editTextAddress.getText()
+                        .toString(), Integer.parseInt(editTextPort
+                        .getText().toString()), response);
+                myClient.execute();
+            }
+        });
+
+        buttonClear.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                response.setText("");
+            }
+        });
+
     // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+    //    TextView tv = (TextView) findViewById(R.id.sample_text);
+    //    tv.setText(stringFromJNI());
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * NATIVE METHODS
      */
     public native String stringFromJNI();
 
